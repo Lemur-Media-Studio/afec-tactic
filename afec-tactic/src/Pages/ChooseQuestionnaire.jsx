f7c27ef593fa4609cd8104ede7cc3cd1d2471a0f
Martin del Rosario <delrosariomartin23@gmail.com>
2023-06-23 17:39:41 +0200

    home updated

--- afec-tactic/src/Pages/ChooseQuestionnaire.jsx
+++ afec-tactic/src/Pages/ChooseQuestionnaire.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Container, Button, Row, Col, Card } from 'react-bootstrap';
+import { Container, Button, Row, Col, Card, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import NavBar from '../Components/NavBar';
 
 function ChooseQuestionnaire() {
 
@@ -12,30 +13,32 @@ function ChooseQuestionnaire() {
 
         <Row className="chooseq-row">
           <Col>
-              <Card className="chooseq-card" style={{ width: '35rem', height:'20rem' }}>
+              <Card className="text-center chooseq-card">
+                <Card.Header className="chooseq-card-header">Cuestionario 1</Card.Header>
                 <Card.Body>
                   <Card.Title className='chooseq-title'>OBJETIVOS DE ENTRENAMIENTO</Card.Title>
                   <Card.Text className="chooseq-p">
-                    En este cuestionario nuestra lA te preguntará cuáles son tus objetivos de entrenamiento para prepararte una sesión con las tareas que
-                    mejor se adapten a tus respuestas.
+                          En este cuestionario nuestra lA te preguntará cuáles son tus objetivos de entrenamiento para prepararte una sesión con las tareas que
+                          mejor se adapten a tus respuestas.
                   </Card.Text>
-                  <Card.Footer><Button className="chooseq-btn mx-3" as={Link} to='/form1-question1'>IR AL CUESTIONARIO</Button></Card.Footer>
                 </Card.Body>
-            </Card>
+                <Card.Footer className="text-muted"><Button className="chooseq-btn mx-3" as={Link} to='/form1-question1'>IR AL CUESTIONARIO</Button></Card.Footer>
+              </Card>
           </Col>
 
           <Col>
-              <Card className="chooseq-card" style={{ width: '35rem', height:'20rem' }}>
+              <Card className="text-center chooseq-card">
+                <Card.Header className="chooseq-card-header">Cuestionario 2</Card.Header>
                 <Card.Body>
                   <Card.Title className='chooseq-title'>PLAN DE PARTIDO</Card.Title>
                   <Card.Text className="chooseq-p">
-                  En este cuestionario nuestra lA te preguntará sobre tú plan de partido en tú último encuentro disputado, junto con otras preguntas valorativas del mismo. 
-                  El objetivo es crearte una sesión con las tareas de entrenamiento que apliquen única y exclusivamente a las áreas de mejora.
+                  En este cuestionario nuestra lA te preguntará sobre tú plan de partido en tú último encuentro disputado, junto con otras preguntas valorativas del mismo. El objetivo es crearte una sesión con las tareas de entrenamiento que apliquen única y exclusivamente a las áreas de mejora.
                   </Card.Text>
-                  <Card.Footer><Button className="chooseq-btn mx-3" as={Link} to='/form2-question1'>IR AL CUESTIONARIO</Button></Card.Footer>
                 </Card.Body>
-            </Card>
+                <Card.Footer className="text-muted"><Button className="chooseq-btn mx-3" as={Link} to='/form2-question1'>IR AL CUESTIONARIO</Button></Card.Footer>
+              </Card>
           </Col>
+
         </Row>
 
 
--- afec-tactic/src/Pages/Home.jsx
+++ afec-tactic/src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { Button } from "react-bootstrap";
+import { Button, Carousel } from "react-bootstrap";
 import Footer from "../Components/Footer";
 import NavBar from "../Components/NavBar";
 
@@ -20,10 +20,97 @@ function Home(){
                         <Button className="home-button">Prueba tu primera semana gratis</Button>
                     </Col>
 
-                    <Col><img src={require('../img/devices.png')} alt="" /></Col>
+                    <Col><img src={require('../img/devices.png')} alt="Imagen de una tablet y un móvil con los logos de AFEC Tactic" /></Col>
                     <Col />
                 </Row>
         </Container>
+
+        <section className="home-sectionblue-container">
+            <Container className="text-center">
+                <h2 className="home-subtitle mx-auto">TACTIC elabora sesiones de entrenamiento <br /> <span className="blue-word">adaptadas 100%</span> a tus objetivos</h2>
+                <Button className="home-button mt-5">Comienza con tu prueba de 7 días gratis</Button>
+            </Container>
+        </section>
+
+        <section className="home-trainingsession-container">
+            <div className="home-grid">
+
+                <Container>
+                
+                <h2 className="home-subtitle text-center mx-auto">TÚ SESIÓN DE ENTRENAMIENTO <br /> EN 3 PASOS</h2>
+                <Row className="home-training-first-row justify-content-center align-items-center">
+                    
+                    <Col>
+                        <img src="https://www.pngall.com/wp-content/uploads/2016/05/Tablet-PNG-HD.png" height="280" alt="Imagen de una tablet y un móvil con los logos de AFEC Tactic" />
+                    </Col>
+
+                    <Col>
+                        <p className="home-p">1. Escoge uno de los cuestionarios</p>
+                    </Col>
+                    
+                </Row>
+
+                <Row className="mt-5 justify-content-center align-items-center">
+                    
+                    <Col>
+                        <p className="home-p">2. Responde a TACTIC todas las preguntas</p>
+                    </Col>
+
+                    <Col>
+                        <img src="https://www.pngall.com/wp-content/uploads/2016/05/Tablet-PNG-HD.png" height="280" alt="Imagen de una tablet y un móvil con los logos de AFEC Tactic" />
+                    </Col>
+                    
+                </Row>
+
+                <Row className="mt-5 justify-content-center align-items-center">
+                    
+                    <Col>
+                        <img src="https://www.pngall.com/wp-content/uploads/2016/05/Tablet-PNG-HD.png" height="280" alt="Imagen de una tablet y un móvil con los logos de AFEC Tactic" />
+                    </Col>
+
+                    <Col>
+                        <p className="home-p">3. Guarda tú sesión de entrenamiento (vídeos e imágenes explicativas)</p>
+                    </Col>
+                    
+                </Row>
+                </Container>
+            </div>
+        </section>
+
+        <section className="home-testimonials-container">
+            <div className="home-grid">
+
+                <Container>
+                
+                <h2 className="home-subtitle-testimonials text-center mx-auto">TESTIMONIOS</h2>
+
+{/*                 <Carousel className="home-carousel">
+
+                        <Carousel.Item>
+                            <h2 className="home-carousel-title">TESTIMONIO 1</h2>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+
+                            </Carousel.Item>
+
+                            <Carousel.Item>
+                            <h2 className="home-carousel-title">TESTIMONIO 2</h2>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </Carousel.Item>
+
+                            <Carousel.Item>
+                            <h2 className="home-carousel-title">TESTIMONIO 3</h2>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Item>
+
+                </Carousel>
+                 */}
+                </Container>
+            </div>
+        </section>
+
 
         <Footer />
       </>
