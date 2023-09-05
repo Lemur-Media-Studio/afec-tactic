9b5da416b969bc338d6985ea5f8d71c6c7c059f3
facumruiz <facundoruiz105@gmail.com>
2023-09-05 09:00:16 -0300

    add validate tokenAPI on login

--- afec-tactic/src/Pages/Login.jsx
+++ afec-tactic/src/Pages/Login.jsx
@@ -3,18 +3,50 @@ import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
 import { Button, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function Login(){
+function Login() {
+    const [user, setUser] = useState("")
+    const [password, setPassword] = useState("")
+    const [loading, setLoading] = useState(false)
+    const navigate = useNavigate()
 
     useEffect(() => {
         document.title = 'Iniciar sesión';
-    
-        return () => {
-          document.title = 'AFEC Tactic';
-        };
-      }, []);
 
-    return(
+        return () => {
+            document.title = 'AFEC Tactic';
+        };
+    }, []);
+    const submit = async (e) => {
+
+        e.preventDefault();
+        setLoading(true)
+
+        try {
+            const response = await fetch('https://afecapp.onrender.com/usuarios/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    user: user,
+                    password: password,
+                })
+            });
+            const data = await response.json();
+            console.log(data);
+        
+
+        } catch (error) {
+            console.log(error)
+
+        }
+
+    }
+
+    return (
 
         <Container>
 
@@ -24,15 +56,15 @@ function Login(){
                     <Card.Title className="card-login-title">INICIAR SESIÓN</Card.Title>
 
                     <Card.Text>
-                        <Form>
+                        <Form onSubmit={submit}>
                             <Form.Group className="mb-3" controlId="formGroupEmail">
                                 <Form.Label>Correo electrónico</Form.Label>
-                                <Form.Control type="email" className="login-input" />
+                                <Form.Control type="email" className="login-input" onChange={(e) => setUser(e.target.value)} />
                             </Form.Group>
 
                             <Form.Group className="mb-3" controlId="formGroupPassword">
                                 <Form.Label>Contraseña</Form.Label>
-                                <Form.Control type="password" className="login-input" />
+                                <Form.Control type="password" className="login-input" onChange={(e) => setPassword(e.target.value)} />
                             </Form.Group>
 
                             <Button className="btn-login" type="submit">
